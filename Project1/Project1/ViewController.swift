9e4e40ed8d8e688de2e789c16c9e00f5c6716d1e
parambhavsar01 <76418793+parambhavsar01@users.noreply.github.com>
2021-03-30 03:38:24 -0700

    Add files via upload

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Param Bhavsar on 26/03/21.
+//  Copyright © 2021 Param Bhavsar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+     
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items{
+            if item.hasPrefix("card") {
+                
+                pictures.append(item)
+                
+            }
+        }
+        
+        print(pictures)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Pictures", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier:
+            "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+            
+        }
+    }
+
+}
+
